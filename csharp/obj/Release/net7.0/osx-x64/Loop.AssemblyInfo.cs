b86d3a4fc2baaa1eee3fc41dc2ec4438c6c91313
wwelden <wwelden@trinity.edu>
2025-03-22 20:33:26 -0500

    update

--- csharp/obj/Release/net7.0/osx-x64/Loop.AssemblyInfo.cs
+++ csharp/obj/Release/net7.0/osx-x64/Loop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f772a5317d3c5aee19d3123ff33dd7ef30c4f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5945dafe20093fac4be273eec641a909c666f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
